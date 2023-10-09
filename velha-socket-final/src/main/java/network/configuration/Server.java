b77e5b9459ce180b7542d66ab4f9a95ee833c0ec
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2023-10-09 14:47:09 -0300

    Updating server to use RMI

--- velha-socket-final/src/main/java/network/configuration/Server.java
+++ velha-socket-final/src/main/java/network/configuration/Server.java
@@ -1,40 +1,22 @@
 package network.configuration;
+import network.handler.ReceivedMessageHandler;
 
-import network.handler.DataFlowHandler;
+import java.rmi.RemoteException;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+public class Server implements DataHandlerInterface {
+    public Server() throws RemoteException {
+        super();
 
-public class Server extends Thread {
-    private final DataFlowHandler dataFlowHandler = DataFlowHandler.getInstance();
-    private Socket socket;
+    }
+    public static DataHandlerInterface remoteObject;
+    private final ReceivedMessageHandler receivedMessageHandler = new ReceivedMessageHandler();
 
-    public void serverSocketConfiguration() {
-        try {
-            int port = 9090;
-            try (ServerSocket serverSocket = new ServerSocket(port)) {
-                System.out.println("Aguardando conexão...");
-                socket = serverSocket.accept();
-                System.out.println("Conexão Estabelecida.");
-            }
-            var outputStream = new DataOutputStream(socket.getOutputStream());
-            this.start();
-            dataFlowHandler.sendMessage(outputStream);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    /**
+     * Message received from the client
+     */
+    @Override
+    public void sendMessage(String message) {
+        receivedMessageHandler.callService(message);
     }
 
-    public void run() {
-        DataInputStream inputStream;
-        try {
-            inputStream = new DataInputStream(socket.getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        dataFlowHandler.receiveMessage(inputStream);
-    }
 }
